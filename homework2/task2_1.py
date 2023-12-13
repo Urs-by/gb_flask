06b8d1e67a87583be6e752a437836d768e191246
Artur.by <urs.by@iclood.com>
2023-12-13 23:20:36 +0300

    Add buttons for tsk 1

--- homework2/task2_1.py
+++ homework2/task2_1.py
@@ -2,25 +2,42 @@
 # 📌 При нажатии на кнопку будет произведена проверка возраста и переход на страницу с результатом или
 # на страницу с ошибкой в случае некорректного возраста.
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+
 @app.route('/task2_1')
 def task1():
     return render_template('task2_1.html')
+
+
+@app.post('/task2_1')
+def send_name():
+    uname = request.form.get('username')
+    age = request.form.get('age')
+    print(uname, age)
+    if age.isdigit() and 10 <= int(age) <= 110:
+        return render_template('task2_1.html', age=age, uname=uname)
+    error = 'Некорректный возраст'
+    return render_template('task2_1.html', error=error)
+
 
 @app.route('/task2_2')
 def task2():
     return render_template('task2_2.html')
 
+
 @app.route('/task2_3')
 def task3():
     return render_template('task2_3.html')
+
 
 @app.route('/task2_4')
 def task4():
--- homework2/wsgi.py
+++ homework2/wsgi.py
@@ -2,4 +2,4 @@ from homework2.task2_1 import app
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
